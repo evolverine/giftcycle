b13a58f856e259532d4a678948f53d79bc2a6ba0
Mihai Chira <mihai.chira@gmail.com>
2012-08-20 17:01:29 +0100

    Addresses #49: using PhoneGap's deviceready event to try to speed up loading. Hopefully it still works, because Chrome doesn't seem to capture that event.

--- js/offerList.js
+++ js/offerList.js
@@ -3,15 +3,24 @@ var offersListComp;
 
 $('#offerListPage').bind('pageinit', function(event) {
 	offersListComp = $('#' + offersListName);
+});
+
+function onDeviceReady()
+{
+	$.mobile.showPageLoadingMsg();
 	
-    var feed = new google.feeds.Feed("http://direct.ilovefreegle.org/rss.php?group=freegle_redbridge");
+	google.setOnLoadCallback(onGoogleFeedsReadyToUse);
+	google.load("feeds", "1");
+}
+
+function onGoogleFeedsReadyToUse()
+{
+	var feed = new google.feeds.Feed("http://direct.ilovefreegle.org/rss.php?group=freegle_redbridge");
     feed.setNumEntries(200);
     feed.setResultFormat(google.feeds.Feed.XML_FORMAT);
     feed.includeHistoricalEntries();
     feed.load(offersLoaded);
-    
-    $.mobile.showPageLoadingMsg();
-});
+}
 
 function offersLoaded(result) {
 	$.mobile.hidePageLoadingMsg();
