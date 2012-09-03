9648be20f6e42e7c7464e66ef17c33e7353540cc
Mihai Chira <mihai.chira@gmail.com>
2012-09-03 11:39:14 +0100

    Addresses #38: removing js code that used to belong to a previous page.

--- js/settingsPage.js
+++ js/settingsPage.js
@@ -1,33 +1,4 @@
-function oldInit(event) {
-	var id = getUrlVars()["id"];
-	$.getJSON(serviceURL + 'getemployee.php?id='+id, displayEmployee);
-}
-
 $('#settingsPage').live('pageshow', init);
 function init(event) {
-	var id = getUrlVars()["id"];
-	$.ajax({
-		url:'fakeOffer_' + id + '.xml',
-		dataType: 'xml',
-		success: displayProduct
-	});
-}
-
-function displayProduct(productXml) {
-	$('#fullName').text($(productXml).find("offer").find("title").text());
-	$('#offerImage').attr('src', 'pics/' + $(productXml).find("offer").find("image").text());
-	$('#offerLocation').text($(productXml).find("offer").find("location").text());
-	$('#offerDescription').text($(productXml).find("offer").find("description").text());
-}
-
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+	console.log("settings pageshow");
 }
