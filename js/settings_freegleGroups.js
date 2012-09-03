750c01cf5dee725ed293bca5ebf0c9eaca072aa6
Mihai Chira <mihai.chira@gmail.com>
2012-09-03 18:46:56 +0100

    Addresses #38: deleting unused js file related to settings.

--- js/settings_freegleGroups.js
+++
@@ -1,5 +0,0 @@
-$('#settings_freegleGroupsPage').live('pageshow', init);
-function init(event) {
-	$("#freegleGroupsList").append('<li>Walthamstow</li>');
-	$("#freegleGroupsList").append('<li>Islington</li>');
-}
